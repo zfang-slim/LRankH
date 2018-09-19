41dd1717ec601b4c1150e14857cd631955509ea5
Zhilong Fang <fangzl@GREEN-NINETY-NINE.MIT.EDU>
2018-09-19 10:31:12 -0400

    up

--- trunk/Experiment/CreateModel/CreateMarmousiIni.m
+++ trunk/Experiment/CreateModel/CreateMarmousiIni.m
@@ -1,6 +1,7 @@
 modeldir  = '/math/home/fangzl/Model/Marmousi-2';
 modelfile = 'marmosi_vp.mat';
-modelfileout = 'marmosi_vp_ini.mat';
+modelfileout1 = 'marmosi_vp_ini_smooth.mat';
+modelfileout2 = 'marmosi_vp_ini_1D.mat';
 curdir    = pwd;
 cd(modeldir);
 
@@ -10,11 +11,14 @@ S1 = opSmooth(n(1), smooth_level);
 S2 = opSmooth(n(2), smooth_level);
 Aout = S1 * A;
 Aout = (S2 * Aout')';
-avec = mean(Aout, 2);
-Aout = repmat(avec, 1, n(2));
+Aout1 = Aout;
+avec  = mean(Aout, 2);
+Aout2 = repmat(avec, 1, n(2));
 
 
 figure;imagesc(A)
-figure;imagesc(Aout);
-WriteAllData(modelfileout, Aout, n, d, o);
+figure;imagesc(Aout1);
+figure;imagesc(Aout2);
+WriteAllData(modelfileout1, Aout1, n, d, o);
+WriteAllData(modelfileout2, Aout2, n, d, o);
 cd(curdir)
